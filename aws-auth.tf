0b4e88de3bdd940ce4f8a1afbce84b5311cba8a2
Manish Khadka <9165231-ManishK_@users.noreply.gitlab.com>
2022-03-17 05:55:21 +0000

    Update aws-auth.tf

--- aws-auth.tf
+++ aws-auth.tf
@@ -28,6 +28,21 @@ locals {
       }
     }]
   })
+
+  map_roles = var.map_roles
+
+  current_auth_configmap = data.kubernetes_config_map.aws-auth
+
+  updated_auth_configmap_data = {
+    data = {
+      mapRoles = replace(yamlencode(
+        distinct(concat(
+          yamldecode(local.current_auth_configmap.data.mapRoles), local.map_roles)
+      )), "\"", "")
+      mapUsers = yamlencode(local.map_users)
+    }
+  }
+
 }
 
 resource "null_resource" "apply" {
@@ -35,9 +50,10 @@ resource "null_resource" "apply" {
     kubeconfig = base64encode(local.kubeconfig)
     cmd_patch  = <<-EOT
       kubectl create configmap aws-auth -n kube-system --kubeconfig <(echo $KUBECONFIG | base64 --decode)
-      kubectl patch configmap/aws-auth --patch "${var.aws_auth_configmap_yaml}" -n kube-system --kubeconfig <(echo $KUBECONFIG | base64 --decode)
+      kubectl patch configmap/aws-auth --type merge --patch "${chomp(jsonencode(local.updated_auth_configmap_data))}" -n kube-system --kubeconfig <(echo $KUBECONFIG | base64 --decode)
     EOT
   }
+  #kubectl patch configmap/aws-auth --patch "${var.aws_auth_configmap_yaml}" -n kube-system --kubeconfig <(echo $KUBECONFIG | base64 --decode)
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
